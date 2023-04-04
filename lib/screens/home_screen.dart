5799cfc6a43110d05342c3461bcebc2d734cd51a
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2023-04-04 10:38:31 +0000

    creation du home page et personnalisation du main

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: const Scaffold(
+        body: Center(
+          child: Text("Bien venu"),
+        ),
+      ),
+    );
+  }
+}
