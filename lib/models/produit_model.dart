c179b5ff2be4722641306356e6b081b7a39510ea
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2023-04-05 23:32:38 +0000

    Ajout du Model

---
+++ lib/models/produit_model.dart
@@ -0,0 +1,50 @@
+import 'package:go2shop/models/category_model.dart';
+
+class ProduitModel {
+  int? id;
+  String? title;
+  int? price;
+  String? description;
+  List<String>? images;
+  String? creationAt;
+  String? updatedAt;
+  CategoriesModel? category;
+
+  ProduitModel(
+      {this.id,
+        this.title,
+        this.price,
+        this.description,
+        this.images,
+        this.creationAt,
+        this.updatedAt,
+        this.category});
+
+  ProduitModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    price = json['price'];
+    description = json['description'];
+    images = json['images'].cast<String>();
+    creationAt = json['creationAt'];
+    updatedAt = json['updatedAt'];
+    category = json['category'] != null
+        ?  CategoriesModel.fromJson(json['category'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['price'] = this.price;
+    data['description'] = this.description;
+    data['images'] = this.images;
+    data['creationAt'] = this.creationAt;
+    data['updatedAt'] = this.updatedAt;
+    if (this.category != null) {
+      data['category'] = this.category!.toJson();
+    }
+    return data;
+  }
+}
